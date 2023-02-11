cff79a051a64ffc9175a7266e5409f1d724a343d
Sebastian Rodriguez <sebaser99@gmail.com>
2023-02-11 12:43:03 -0500

    cambiamos estilos del about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,23 +2,26 @@ import Link from 'next/link'
 import { MainLayout } from '@/components/layouts/MainLayout'
 import { DarkLayout } from '@/components/layouts/DarkLayout'
 
-
+const margin = {
+  margin: '2rem 0',
+  textAlign: 'center' as const
+}
 
 export default function About() {
   return (
 
       <>
-        <h1>About Page</h1>
-        <h1 className='title'>
+        <h1 style={margin}>About Page</h1>
+        <h1 style={margin} className='title'>
           Ir a <Link href="/">Home</Link>
         </h1>
-        <h1 className='title'>
+        <h1 style={margin} className='title'>
           Ir a <Link href="/contact">Contact</Link>
         </h1>
-        <h1 className='title'>
+        <h1 style={margin} className='title'>
           Ir a <Link href="/pricing">Pricing</Link>
         </h1>
-        <p className='description'>
+        <p style={margin} className='description'>
           Get Started by editing{''}
           <code className='code'>  pages/about.tsx</code>
         </p>
